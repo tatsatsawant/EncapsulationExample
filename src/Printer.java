1df33eaf597ba3d778b25bbb1d3eade9415c4183
Tatsat Sawant <tatsatsawant6@gmail.com>
2024-01-18 11:47:14 +0530

    Created project

---
+++ src/Printer.java
@@ -0,0 +1,31 @@
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.pagesPrinted = 0;
+        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : -1;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        int tempAmount = tonerLevel + tonerAmount;
+        if (tempAmount < 0 || tempAmount > 100) {
+            return -1;
+        }
+        tonerLevel += tonerAmount;
+        return tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        int jobPages = (duplex) ? (pages / 2) + (pages % 2) : pages;
+        pagesPrinted += jobPages;
+        return jobPages;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
